02e3d23c71a0232d0a549109adda4c5d0c890bcf
Edwin Ko <edwin.mk.ko@gmail.com>
2023-11-27 09:14:49 +0000

    Add login cookie

--- express_server.js
+++ express_server.js
@@ -64,6 +64,11 @@ app.post("/urls/:id", (req, res) => {
   res.redirect(`/urls`);
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect(`/urls`);
+});
+
 const generateRandomString = () => {
   const alphanumericChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let str = "";
